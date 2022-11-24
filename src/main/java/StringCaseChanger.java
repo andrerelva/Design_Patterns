f9755148296cf8b6f6c9851b5ef8304df324c1a3
André Relva <up202108695@up.pt>
2022-11-24 14:54:48 +0000

    Implemented StringCaseChanger and respective test class

---
+++ src/main/java/StringCaseChanger.java
@@ -0,0 +1,10 @@
+public class StringCaseChanger implements StringTransformer {
+    public void execute(StringDrink drink) {
+        char[] text = drink.getText().toCharArray();
+        StringBuilder ret = new StringBuilder();
+        for (char c : text) {
+            ret.append(Character.isLowerCase(c) ? Character.toUpperCase(c) : Character.toLowerCase(c));
+        }
+        drink.setText(ret.toString());
+    }
+}
