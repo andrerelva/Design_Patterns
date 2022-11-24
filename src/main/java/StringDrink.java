cc5ec1481a2851143ccbe299ee24c8b08609424b
André Relva <up202108695@up.pt>
2022-11-24 14:27:12 +0000

    Implemented StringDrink and StringDrinkTest classes

---
+++ src/main/java/StringDrink.java
@@ -0,0 +1,12 @@
+public class StringDrink {
+    private String text;
+    public StringDrink(String s) { text = s; }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
